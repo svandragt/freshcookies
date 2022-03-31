2c36d4564f1e507aa63ff93328ee8f978f228c11
Sander van Dragt <sander@vandragt.com>
2022-03-31 14:07:54 +0100

    JS formatting.

--- background.js
+++ background.js
@@ -1,20 +1,25 @@
 const _browser = browser || chrome;
 
 _browser.cookies.onChanged.addListener(cookieChanged);
-console.info('Fresh Cookies loaded.')
+
+console.info('Fresh Cookies loaded.');
+
+function isExpiredChangeEvent(changeInfo) {
+    return changeInfo.cause == "expired" || changeInfo.cause == "evicted" || changeInfo.removed;
+}
 
 function cookieChanged(changeInfo) {
-    if (changeInfo.cause == "expired" || changeInfo.cause == "evicted" || changeInfo.removed) {
+    if (isExpiredChangeEvent(changeInfo)) {
         return;
     }
 
     const cookie = changeInfo.cookie;
-    const maxCookieAge = 15; // days
-    const bufferTime = 60 * 10 // minutes
+    const maxCookieAgeDays = 15;
+    const bufferTimeMinutes = 10;
 
-    const maxAllowedExpiration = Math.round((new Date).getTime() / 1000) + (maxCookieAge * 3600 * 24);
+    const maxAllowedExpiration = Math.round((new Date).getTime() / 1000) + (maxCookieAgeDays * 3600 * 24);
 
-    if (!cookie.session && cookie.expirationDate != undefined && cookie.expirationDate > maxAllowedExpiration + bufferTime) {
+    if (!cookie.session && cookie.expirationDate != undefined && cookie.expirationDate > maxAllowedExpiration + (bufferTimeMinutes * 60)) {
         // TODO can I just clone cookie and amend?
         var newCookie = {};
         newCookie.name = cookie.name;
